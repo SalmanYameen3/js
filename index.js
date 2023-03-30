433bac8263a1a9d00057c99e8f6ff857cc088a7e
SalmanYameen3 <salmanyameen@gmail.com>
2023-03-30 04:40:30 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,119 @@
+alert ( "Thanks for your input!" );
+
+
+
+
+var name = "Mark";
+console.log(name)
+
+var name = "Mark";
+var name = "Mark";
+
+
+
+var nationality;
+nationality = "U.S.";
+console.log(nationality)
+
+
+
+var floogle = "Mark";
+console.log(floogle)
+
+
+var name = "Mark";
+
+
+
+var lessonAuthor = "Mark";
+var guyWhoKeepsSayingHisOwnName = "Mark";
+var x = "Mark";
+
+
+
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+console.log("aussieGreeting")
+
+
+
+
+var nickname = "Bub";
+alert("Thanks for your input!");
+
+
+
+
+var thanx = "Thanks for your input!"
+alert(thanx);
+
+
+
+var weight = 150;
+ var x= weight + 25
+ console.log(x)
+
+
+
+ 
+ var originalNum = 23;
+ var newNum = originalNum + 7;
+ console.log(newNum)
+
+
+
+
+  var originalNum = 23;
+ var numToBeAdded = 7;
+ var newNum = originalNum + numToBeAdded;
+console.log(newNum)
+
+
+  var originalNum = 90;
+ originalNum = originalNum + 10;
+ console.log(originalNumv)
+
+
+
+
+  var originalNum = "23";
+ var newNum = originalNum + 7;
+
+
+ var spec = prompt("Your species?", "human");
+
+
+
+
+
+ var question = "Your species?";
+ var defaultAnswer = "human";
+ var spec = prompt(question, defaultAnswer);
+
+
+
+
+ var numberOfCats = prompt("How many cats?");
+  var tooManyCats = numberOfCats + 1;
+
+
+   var x = prompt("Where does the Pope live?");
+   if (x === "Vatican") {
+   alert("Correct!");
+   }
+
+
+
+    if (x === "Vatican") {
+     alert("Correct!");
+     }
+     else {
+     alert("Wrong answer");
+     }
+    
+
+
+
+
+  
